f211bac85a87feebc3d9b7eee8c2763e4bf11587
Ignacio Aguilera <Ignacio Aguilera@DESKTOP-RDC8Q6I>
2026-01-23 17:10:04 -0300

    fix package twilio fastify

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,22 @@
 import "./env";
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import formbody from "@fastify/formbody";
+
 import { chatRoutes } from "./routes/chat";
-//import { whatsappWebhook } from "./webhook"; //OLD
 import { whatsappTwilioWebhook } from "./webhook-twilio";
 
 async function startServer() {
     const app = Fastify({ logger: true });
 
     await app.register(cors, { origin: "*" });
+    await app.register(formbody); // ⬅️ CLAVE PARA TWILIO
 
-    //whatsappWebhook(app); //OLD
     whatsappTwilioWebhook(app);
-
     await chatRoutes(app);
 
     app.get("/health", async () => ({ status: "ok" }));
 
     const port = Number(process.env.PORT) || 3000;
-
     await app.listen({ port, host: "0.0.0.0" });
-
-    console.log(`\n🚀 Backend running:`);
-    console.log(`👉 http://localhost:${port}/health\n`);
 }
-
-startServer().catch((err) => {
-    console.error("❌ Error starting server:", err);
-    process.exit(1);
-});
